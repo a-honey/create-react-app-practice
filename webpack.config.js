4a7752f651ab67fdf9c18c379066f6d956f57926
a-honey <jungahyeon0512@khu.ac.kr>
2023-11-07 05:47:10 +0900

    npm start

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
