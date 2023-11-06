bd7201255f844ea659e22b8da827aaebe15e9222
a-honey <jungahyeon0512@khu.ac.kr>
2023-11-07 05:57:23 +0900

    Add sass

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         exclude: /node_modules/,
         use: { loader: "babel-loader" },
       },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
     ],
   },
   resolve: {
